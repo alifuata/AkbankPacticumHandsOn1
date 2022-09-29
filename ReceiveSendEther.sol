e272eabd136600f0d7cf01b5677477851eeab28f
alifuata <53005120+alifuata@users.noreply.github.com>
2022-09-30 00:07:59 +0300

    Update ReceiveSendEther.sol

--- ReceiveSendEther.sol
+++ ReceiveSendEther.sol
@@ -1,15 +1,19 @@
 //SPDX-License-Identifier:MIT
 pragma solidity 0.8.17;
 
-contract AfaCollector{
+contract Collector{
+//variables to keep information
     address public owner;
     uint256 public balance;
+    //initialize owner variable
     constructor(){
         owner=msg.sender;
     }
+    //get some funds to the account
     receive() payable external {
         balance+=msg.value;
     }
+    //send some ether with security features
     function withdraw(uint amount, address payable destination)public{
         require(msg.sender==owner,"Only owner can withdraw");
         require(amount<=balance,"Insufficient funds");
